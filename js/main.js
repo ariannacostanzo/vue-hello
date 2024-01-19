c8a4dd921c8231092b23cd9fea8dd3ecdf04a636
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-19 13:47:57 +0100

    implementing bonus

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ const app = createApp({
     name: 'Vue Hello',
     data() {
         return {
-            message: 'Sto usando Vue per la prima volta'
+            message: 'Sto usando Vue per la prima volta',
+            image: {
+                name: 'Vue',
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
+            }
+            
         }
     }
 })
